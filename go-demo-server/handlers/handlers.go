705274a8f3255b3b5a93c999d6d979cc8738b097
Rossi Sun <zanmato1984@gmail.com>
2025-05-15 11:52:58 -0700

    Add some sample attributes

--- go-demo-server/handlers/handlers.go
+++ go-demo-server/handlers/handlers.go
@@ -5,12 +5,22 @@ import (
 	"net/http"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 func WorkHandler(w http.ResponseWriter, r *http.Request) {
 	tr := otel.Tracer("go-demo-server")
 	_, span := tr.Start(r.Context(), "work-handler")
 	defer span.End()
+
+	// Add useful span attributes
+	span.SetAttributes(
+		attribute.String("http.method", r.Method),
+		attribute.String("http.url", r.URL.String()),
+		attribute.String("user_agent", r.UserAgent()),
+		attribute.String("client_ip", r.RemoteAddr),
+	)
+
 	// Simulate work
 	fmt.Fprintln(w, "Work done!")
 }
